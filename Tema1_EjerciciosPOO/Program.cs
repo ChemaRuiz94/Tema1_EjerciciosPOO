6a13b567cb87ea108f475705c6b90538b098d3fc
Chema ruiz <Chemaruiz94@gmail.com>
2021-09-20 12:35:39 +0200

    Commit - Creación del Menú en Main

--- Tema1_EjerciciosPOO/Program.cs
+++ Tema1_EjerciciosPOO/Program.cs
@@ -6,7 +6,29 @@ namespace Tema1_EjerciciosPOO
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("EJERCICIOS TEMA 1 - CHEMA RUIZ");
+            int op;
+            do {
+                Console.WriteLine("______________________________");
+                Console.WriteLine("1 -> Manejar Fechas");
+                Console.WriteLine("2 -> Clase Libros");
+                Console.WriteLine("3 -> Alquiler Puerto");
+                Console.WriteLine("4 -> Salir");
+                Console.WriteLine("Escoge una opcion: ");
+                Console.WriteLine("______________________________");
+
+                op = (int)Console.Read();
+
+                if (op >= 0 && op <= 4)
+                {
+                    //Aqui va el menú
+                }
+                else 
+                {
+                    Console.WriteLine("Introduce una opcion correcta")
+                };
+            } while (op == 4);
+
         }
     }
 }
