8aa35944c5dfc60fffc4785a6aa4cc85a91d0f51
Chema ruiz <Chemaruiz94@gmail.com>
2021-09-20 16:48:39 +0200

    Commit-Fechas opcion 1

--- Tema1_EjerciciosPOO/ManejoFechas/MainFechas.cs
+++ Tema1_EjerciciosPOO/ManejoFechas/MainFechas.cs
@@ -8,8 +8,8 @@ namespace Tema1_EjerciciosPOO.ManejoFechas
 {
     class MainFechas
     {
-        public MainFechas() 
-        { 
+        public MainFechas()
+        {
         }
 
         public void pintarMenu()
@@ -17,17 +17,106 @@ namespace Tema1_EjerciciosPOO.ManejoFechas
             Console.WriteLine("EJERCICIO MANEJAR FECHAS");
             Console.WriteLine("______________________________");
             Console.WriteLine("1 -> DIA DE LA SEMANA");
-            Console.WriteLine("(Introducir una fecha y decir a que dia de la semana corresponde)");
+            Console.WriteLine("     (Introducir una fecha y decir a que dia de la semana corresponde)");
             Console.WriteLine("2 -> INCREMENTAR FECHA");
-            Console.WriteLine("(Introducir una fecha e incrementarla en un numero de dias dado por el usuario)");
+            Console.WriteLine("     (Introducir una fecha e incrementarla en un numero de dias dado por el usuario)");
             Console.WriteLine("3 -> DIFERENCIAR FECHAS");
-            Console.WriteLine("(Introduces  dos  fechas  e  indicas  el  tiempo  en  años,  meses  y  días pasados entre las dos fechas)");
+            Console.WriteLine("     (Introduces  dos  fechas  e  indicas  el  tiempo  en  años,  meses  y  días pasados entre las dos fechas)");
             Console.WriteLine("4 -> COMPARAR FECHAS");
-            Console.WriteLine("(Compara dos fechas e indica si la primera es mayor, menor o igual que la segunda fecha)");
+            Console.WriteLine("     (Compara dos fechas e indica si la primera es mayor, menor o igual que la segunda fecha)");
             Console.WriteLine("5 -> MOSTRAR FECHAS EN FORMATO LARGO");
-            Console.WriteLine("(Introduce una fecha en formato corto (“dd/mm/aa”)) y se devuelve en formato largo");
+            Console.WriteLine("     (Introduce una fecha en formato corto (“dd/mm/aa”)) y se devuelve en formato largo");
+            Console.WriteLine("______________________________");
+            Console.WriteLine("ESCOGE UNA OPCION DE ESTE EJERCICIO: ");
             Console.WriteLine("______________________________");
 
+        }
+
+        public void menu()
+        {
+            int op = PedirOpcionUsuario();
+            switch (op)
+            {
+                case 1:
+                    Console.WriteLine("Opcion 1");
+                    diaDeLaSemana();
+                    break;
+                case 2:
+                    Console.WriteLine("Opcion 2");
+                    break;
+                case 3:
+                    Console.WriteLine("Opcion 4");
+                    break;
+                case 4:
+                    Console.WriteLine("Opcion 3");
+                    break;
+                case 5:
+                    Console.WriteLine("Opcion 5");
+                    break;
+                default:
+                    Console.WriteLine("Opcion Incorrecta");
+                    break;
+
+            }
+        }
+
+        public static void diaDeLaSemana()
+        {
+
+            DateTime fecha = pedirFechaUsuario();
+
+            Console.WriteLine("Esa fecha corresponde a " + fecha.DayOfWeek.ToString());
+
+        }
+
+        /**
+         * Metodo para pedir  datos al usuario 
+         * y devolver la opcion del menu
+         */
+        public static int PedirOpcionUsuario()
+        {
+            return int.Parse(Console.ReadLine());
+        }
+
+        /**
+         * Metodo para pedir  fecha al usuario 
+         * y devolver la una fecha con formato
+         */
+        public static DateTime pedirFechaUsuario()
+        {
+            int anio;
+            int mes;
+            int dia;
+            DateTime fecha = new DateTime();
+
+            try
+            {
+                do
+                {
+                    Console.WriteLine("Introduce el año");
+                    anio = int.Parse(Console.ReadLine());
+                } while (anio < 1 || anio > 9999);
+
+                do
+                {
+                    Console.WriteLine("Introduce el mes");
+                    mes = int.Parse(Console.ReadLine());
+                } while (mes < 1 || mes > 12);
+
+                do
+                {
+                    Console.WriteLine("Introduce el dia");
+                    dia = int.Parse(Console.ReadLine());
+                } while (dia < 1 || dia > 31);
+
+                fecha = new DateTime(anio, mes, dia);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Fecha introducida erronea");
+                throw;
+            }
+            return fecha;
         }
     }
 }
--- Tema1_EjerciciosPOO/Program.cs
+++ Tema1_EjerciciosPOO/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 
+
+
+
 namespace Tema1_EjerciciosPOO
 {
     class Program
@@ -7,28 +10,58 @@ namespace Tema1_EjerciciosPOO
         static void Main(string[] args)
         {
             Console.WriteLine("EJERCICIOS TEMA 1 - CHEMA RUIZ");
-            int op;
-            do {
+            int op = 0;
+            bool salir = false;
+            do
+            {
                 Console.WriteLine("______________________________");
                 Console.WriteLine("1 -> Manejar Fechas");
                 Console.WriteLine("2 -> Clase Libros");
                 Console.WriteLine("3 -> Alquiler Puerto");
                 Console.WriteLine("4 -> Salir");
-                Console.WriteLine("Escoge una opcion: ");
                 Console.WriteLine("______________________________");
 
-                op = (int)Console.Read();
+                Console.WriteLine("Escoge una opcion: ");
+                Console.WriteLine("______________________________");
+                op = PedirOpcionUsuario();
 
                 if (op >= 0 && op <= 4)
                 {
-                    //Aqui va el menú
-                }
-                else 
-                {
-                    Console.WriteLine("Introduce una opcion correcta")
-                };
-            } while (op == 4);
+                    switch (op)
+                    {
+                        case 1:
+                            Console.WriteLine("Opcion 1");
+                            ManejoFechas.MainFechas menu = new ManejoFechas.MainFechas();
+                            menu.pintarMenu();
+                            menu.menu();
+                            break;
+                        case 2:
+                            Console.WriteLine("Opcion 2");
+                            break;
+                        case 3:
+                            Console.WriteLine("Opcion 3");
+                            break;
+                        case 4:
+                            Console.WriteLine("Hasta pronto!");
+                            salir = true;
+                            break;
+                        default:
+                            Console.WriteLine("Opcion Incorrecta");
+                            break;
 
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Introduce una opcion correcta");
+                }
+            } while (!salir);
+
+        }
+
+        public static int PedirOpcionUsuario()
+        {
+            return int.Parse(Console.ReadLine());
         }
     }
 }
